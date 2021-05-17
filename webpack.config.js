ac745ac54945eb532055b638c071d1150e40e9b1
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-05-17 15:09:25 +0300

    Install babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,5 +11,22 @@ module.exports = {
   output: {
     filename: "index.js",
     path: path.resolve(__dirname, "dist"),
+    environment: {
+      arrowFunction: false,
+    },
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 };
