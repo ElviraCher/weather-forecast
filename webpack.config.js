871f334d196f63e4d8fb33be5464e85033d3aaef
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-05-17 22:56:38 +0300

    Simple eslint-fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,14 +21,12 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [
-                ['@babel/preset-env', { targets: "ie 11" }]
-            ]
-          }
-        }
-      }
-    ]
-  }
+            presets: [["@babel/preset-env", { targets: "ie 11" }]],
+          },
+        },
+      },
+    ],
+  },
 };
