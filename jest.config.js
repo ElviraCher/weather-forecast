dc388700038c6de0bbbd4a6c917c27d90d9eab13
Elvira Cherkasova <ellisha25sheri@gmail.com>
2021-07-02 23:21:27 +0300

    Change coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -44,10 +44,10 @@ module.exports = {
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
-      branches: 30,
-      functions: 30,
-      lines: 30,
-      statements: 30,
+      branches: 60,
+      functions: 58,
+      lines: 60,
+      statements: 60,
     },
   },
 
